2472b95784743d73eafea745a55d198841c708f8
Samaze123456 <92528417+Samaze123456@users.noreply.github.com>
2021-10-14 16:31:54 +0200

    Update Adn_TP04.js

--- Adn_TP04.js
+++ Adn_TP04.js
@@ -1,3 +1,5 @@
+
+const LETTREADN = ['A', 'T', 'G', 'C'];
 let compteurBouclePrincipale = 0;
 let testBouclePrincipale = true;
 let compteurBoucleSecondaire = 0;
@@ -6,7 +8,6 @@ let compteurBoucleFin = 0;
 let compteurFin = 0;
 let testFin = true;
 let nbreChaineTrouvee = 0;
-const lettreAdn = ['A', 'T', 'G', 'C'];
 let adnPrincipale = prompt("Quelle est votre chaine principale ?");
 let adnSecondaire = prompt("Quelle est votre seconde chaine à tester ?");
 
@@ -14,7 +15,7 @@ if (adnSecondaire < adnPrincipale)
 {
     while ((compteurBouclePrincipale < adnPrincipale.length) && (testBouclePrincipale == true))
     {
-        if (lettreAdn.indexOf(adnPrincipale[compteurBouclePrincipale]) == -1)
+        if (LETTREADN.indexOf(adnPrincipale[compteurBouclePrincipale]) == -1)
         {
             testBouclePrincipale = false;
             console.log("Vous n'avez pas entré une chaine d'ADN correcte");
@@ -23,7 +24,7 @@ if (adnSecondaire < adnPrincipale)
     }
     while ((compteurBoucleSecondaire < adnSecondaire.length && (testBoucleSecondaire == true)))
     {
-        if (lettreAdn.indexOf(adnSecondaire[compteurBoucleSecondaire]) == -1)
+        if (LETTREADN.indexOf(adnSecondaire[compteurBoucleSecondaire]) == -1)
         {
             testBoucleSecondaire = false;
             console.log("Vous n'avez pas entré une chaine d'ADN correcte");
