6809b404b418dd8d382cb082f66f9b60c5a5bd0f
TonyRasoa <92527255+TonyRasoa@users.noreply.github.com>
2021-10-14 15:42:52 +0200

    Update Adn_TP04.js

--- Adn_TP04.js
+++ Adn_TP04.js
@@ -10,51 +10,46 @@ let testFin = true;
 let nbreChaineTrouvee = 0;
 const lettreAdn = ['A', 'T', 'G', 'C'];
 adnPrincipale = prompt("Quelle est votre chaine principale ?"),
-adnSecondaire = prompt("Quelle est votre seconde chaine à tester ?");
+    adnSecondaire = prompt("Quelle est votre seconde chaine à tester ?");
 if (adnSecondaire < adnPrimaire)
 {
-	while ((compteurBouclePrincipale < adnPrincipale.length) && (testBouclePrincipale == true))
-	{
-		if (lettreAdn.indexOf(adnPrincipale[compteurBouclePrincipale]) == -1)
-		{
-			testBouclePrincipale = false;
-			console.log("Vous n'avez pas entré une chaine d'ADN correcte");
-		}
-		compteurBouclePrincipale++;
-	}
-	while ((compteurBoucleSecondaire < adnSecondaire.length) && (testBoucleSecondaire == true))
-	{
-		if (lettreAdn.indexOf(adnSecondaire[compteurBoucleSecondaire]) == -1)
-		{
-			testBoucleSecondaire = false;
-			console.log("Vous n'avez pas entré une chaine d'ADN correcte");
-		}
-		compteurBoucleSecondaire++;
-	}
+    while ((compteurBouclePrincipale < adnPrincipale.length) && (testBouclePrincipale == true))
+    {
+        if (lettreAdn.indexOf(adnPrincipale[compteurBouclePrincipale]) == -1)
+        {
+            testBouclePrincipale = false;
+            console.log("Vous n'avez pas entré une chaine d'ADN correcte");
+        }
+        compteurBouclePrincipale++;
+    }
+    while ((compteurBoucleSecondaire < adnSecondaire.length && (testBoucleSecondaire == true)))
+    {
+        if (lettreAdn.indexOf(adnSecondaire[compteurBoucleSecondaire]) == -1)
+        {
+            testBoucleSecondaire = false;
+            console.log("Vous n'avez pas entré une chaine d'ADN correcte");
+        }
+        compteurBoucleSecondaire++;
+    }
 }
 else
 {
-	console.log("La chaine secondaire est plus grande que la chaine principale.");
+    console.log("La chaine secondaire est plus grande que la chaine principale.");
 }
 while (compteurBoucleFin < adnPrincipale.length)
 {
-	if (adnPrincipale[compteurBoucleFin] == adnSecondaire[0])
-	{
-		compteurFin = 0;
-		testFin = true;
-		while (compteurFin < adnSecondaire.length && testFin == true)
-		{
-			if (adnPrincipale[compteurBoucleFin+compteurFin] == adnSecondaire[compteurFin])
-			{
-				compteurFin++;				
-			}
-			else
-			{
-				testFin = false;
-			}
-		}
-		if (compteurFin = adnSecondaire.length)
-		{
-			nbreChaineTrouvee++;
-	}
+    if (adnPrincipale[compteurBoucleFin] == adnSecondaire[0]) {
+        compteurFin = 0;
+        testFin = true;
+        while (compteurFin < adnSecondaire.length && testFin == true) {
+            if (adnPrincipale[compteurBoucleFin + compteurFin] == adnSecondaire[compteurFin]) {
+                compteurFin++;
+            } else {
+                testFin = false;
+            }
+        }
+        if (compteurFin = adnSecondaire.length) {
+            nbreChaineTrouvee++;
+        }
+    }
 }
